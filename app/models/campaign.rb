e12855524a3e3781b1f6072e75e70fca2d8ae4f6
github username <github email address>
2021-03-29 18:30:40 -0500

    campaign name change

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -1,4 +1,4 @@
-class Table < ApplicationRecord
+class Campaign < ApplicationRecord
     has_many :characters
     has_many :enemies
 end
--- db/migrate/20210329143210_create_tables.rb
+++
@@ -1,8 +0,0 @@
-class CreateTables < ActiveRecord::Migration[6.1]
-  def change
-    create_table :tables do |t|
-
-      t.timestamps
-    end
-  end
-end
