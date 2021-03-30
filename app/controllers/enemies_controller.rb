e1cc077d22b7c0a85821a71138454e2b539d4904
Nestor <nestor.venegas95@gmail.com>
2021-03-29 20:58:11 -0500

    started enemy crud

--- app/controllers/enemies_controller.rb
+++ app/controllers/enemies_controller.rb
@@ -1,2 +1,33 @@
 class EnemiesController < ApplicationController
+    before_action :find_enemy, only [:show, :edit, :update]
+
+    def index
+        @enemies = Enemy.all
+    end
+
+    def new
+        @enemy = Enemy.new
+    end
+
+    def create 
+       @enemy = Enemy.create(enemy_params)
+       @enemy.save
+       redirect_to @enemy
+    end
+
+    def update
+        @enemy.update(enemy_params)
+    end
+
+
+    private 
+
+        def find_enemy
+            @enemy = Enemy.find(params[:id])
+        end
+
+        def enemy_params
+            params.require(:enemy).permit(:strength, :dexterity, :constitution, :intelligence, :wisdom, :charisma, :armor_rating, :challenge_rating, :max_hp, :current_hp, :experience_value)
+        end
+
 end
