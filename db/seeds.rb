b9a6dd98773dd41cf58fc66adc4805ae3038b833
Nestor <nestor.venegas95@gmail.com>
2021-04-01 13:00:13 -0500

    combat functionality

--- db/seeds.rb
+++ db/seeds.rb
@@ -34,21 +34,6 @@ shield6 = Item.create(name: "N95 Masks", cost: "800", weight: 12, armor: 25)
 
 
 usr1 = User.create(username: "emily13", password: "milk")
-<<<<<<< HEAD
-usr2 = User.create(username:"my oofs tho", password_digest: "oof")
-
-e1 = Enemy.create( name: "oof" , strength: rand(0..20) , dexterity: rand(0..20) , constitution: rand(0..20) , intelligence: rand(0..20) , wisdom: rand(0..20) , charisma: rand(0..20) , armor_rating: rand(0..10), max_hp: 3 , current_hp: 3 ,  campaign: cam1 , user: usr1 )
-e2 = Enemy.create( name: "oofer" , strength: rand(0..20) , dexterity: rand(0..20) , constitution: rand(0..20) , intelligence: rand(0..20) , wisdom: rand(0..20) , charisma: rand(0..20) , armor_rating: rand(0..10), max_hp: 3 , current_hp: 3,  campaign: cam1 , user: usr1)
-e3 = Enemy.create( name: "oofest" , strength: rand(0..20) , dexterity: rand(0..20) , constitution: rand(0..20) , intelligence: rand(0..20) , wisdom: rand(0..20) , charisma: rand(0..20) , armor_rating: rand(0..10), max_hp: 3 , current_hp: 3,   campaign: cam1 , user: usr1)
-e4 = Enemy.create( name: "oofy" , strength: rand(0..20) , dexterity: rand(0..20) , constitution: rand(0..20) , intelligence: rand(0..20) , wisdom: rand(0..20) , charisma: rand(0..20) , armor_rating: rand(0..10), max_hp: 3 , current_hp: 3,  campaign: cam2 , user: usr1)
-e5 = Enemy.create( name: "oofem" , strength: rand(0..20) , dexterity: rand(0..20) , constitution: rand(0..20) , intelligence: rand(0..20) , wisdom: rand(0..20) , charisma: rand(0..20) , armor_rating: rand(0..10), max_hp: 3 , current_hp: 3 ,  campaign: cam2 , user: usr1)
-
-c1 = Character.create( name: "foo" , strength: rand(0..20) , dexterity: rand(0..20) , constitution: rand(0..20) , intelligence: rand(0..20) , wisdom: rand(0..20) , charisma: rand(0..20) , max_hp: 3 , current_hp: 3 ,  campaign: cam1 , user: usr1 )
-c2 = Character.create( name: "refoo" , strength: rand(0..20) , dexterity: rand(0..20) , constitution: rand(0..20) , intelligence: rand(0..20) , wisdom: rand(0..20) , charisma: rand(0..20) , max_hp: 3 , current_hp: 3 ,  campaign: cam1 , user: usr1)
-c3 = Character.create( name: "tsefoo" , strength: rand(0..20) , dexterity: rand(0..20) , constitution: rand(0..20) , intelligence: rand(0..20) , wisdom: rand(0..20) , charisma: rand(0..20) , max_hp: 3 , current_hp: 3 ,  campaign: cam1 , user: usr1)
-c4 = Character.create( name: "yfoo" , strength: rand(0..20) , dexterity: rand(0..20) , constitution: rand(0..20) , intelligence: rand(0..20) , wisdom: rand(0..20) , charisma: rand(0..20) , max_hp: 3 , current_hp: 3 , campaign: cam2 , user: usr1)
-c5 = Character.create( name: "mefoo" , strength: rand(0..20) , dexterity: rand(0..20) , constitution: rand(0..20) , intelligence: rand(0..20) , wisdom: rand(0..20) , charisma: rand(0..20) , max_hp: 3 , current_hp: 3 ,  campaign: cam2 , user: usr1)
-=======
 
 cam1 = Campaign.create(title: "land of the oof")
 cam2 = Campaign.create(title: "oof em port")
@@ -66,7 +51,6 @@ c2 = Character.create( name: "Nestor" , strength: rand(0..20) , dexterity: rand(
 c3 = Character.create( name: "Nestor" , strength: rand(0..20) , dexterity: rand(0..20) , constitution: rand(0..20) , intelligence: rand(0..20) , wisdom: rand(0..20) , charisma: rand(0..20) , armor_rating: rand(0..10), max_hp: 3 , current_hp: 3 , experience: rand(0..20),  campaign: cam1 , user: usr1)
 c4 = Character.create( name: "Jose" , strength: rand(0..20) , dexterity: rand(0..20) , constitution: rand(0..20) , intelligence: rand(0..20) , wisdom: rand(0..20) , charisma: rand(0..20) , armor_rating: rand(0..10), max_hp: 3 , current_hp: 3 , experience: rand(0..20), campaign: cam2 , user: usr1)
 c5 = Character.create( name: "Adri" , strength: rand(0..20) , dexterity: rand(0..20) , constitution: rand(0..20) , intelligence: rand(0..20) , wisdom: rand(0..20) , charisma: rand(0..20) , armor_rating: rand(0..10), max_hp: 3 , current_hp: 3 , experience: rand(0..20),  campaign: cam2 , user: usr1)
->>>>>>> master
 
 
 encounter_1 = Encounter.create(character: c1, enemy: e1)
